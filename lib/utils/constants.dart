6970161b0e8fd3cbca0f73b5fe64f5ecb7447a6e
Sahil Gupta <71605147+isahilgupta1306@users.noreply.github.com>
2022-04-02 13:54:41 +0530

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-String infura_url =
-    "https://kovan.infura.io/v3/ca664bf5de8c419c94c6bd0ad637b2a7";
+import 'package:flutter/material.dart';
 
-String owner_privKey =
-    "30a710c6f359e2d3fa4463f2007d6795b5a2b5b0828b5967cc50bce33d1d711a";
-String voter_privKey =
-    "1db52277a4bb04069f5cfc600a59c0644ec681bb9956da6f59423d92c940bdc1";
-String contract_address = "0x298288532519f0b09d6aCdBE5196312757dB2496";
+String infura_url = "YOUR_INFURA_URL";
+
+String owner_privKey = "OWNERS_PRIV_KEY";
+String voter_privKey = "VOTERS_PRIC_KEY";
+String contract_address = "CONTRACT_HASH_ADDRESS";
+
